94bdcd08708b109b718e9762b23cb915c522ded3
Farhan-Rad <farhanrasch15@gmail.com>
2025-09-29 10:46:24 +0800

    update

--- LGC/PENGAWAS_KSP/index.php
+++ LGC/PENGAWAS_KSP/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KSP Manager</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body class="bg-slate-50 min-h-screen text-slate-800">
     <header class="bg-emerald-500 p-6 shadow-md">
